132d0deb9a3ca17817f3ed54725cf89d80e1f00b
Julia <bartleyjulia@gmail.com>
2018-03-02 03:06:53 +0000

    Implemented order completion information

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,10 @@
 class OrdersController < ApplicationController
 
+
+
   def show
-    @order = Order.find(params[:id])
+    @order = Order.includes(:line_items => [:product]).find(params[:id])
+    # @line_items = LineItem.find(params[:id])
   end
 
   def create
